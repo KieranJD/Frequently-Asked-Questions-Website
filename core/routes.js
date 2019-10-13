db45ccf946a90f1feb7a837f4f71c4009db83d6a
Wallef Reis Borges <reisborw@coventry.ac.uk>
2019-10-13 12:24:56 +0100

    creating database

--- core/routes.js
+++ core/routes.js
@@ -1,8 +1,10 @@
-'use strict'
+'use strict';
 
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/', ctx => ctx.body = `Testing routes file`)
+router.get('/', async ctx => {
+    ctx.body = `Testing routes file`;
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-'use strict'
+'use strict';
 
 require('dotenv').config();
 const Koa = require('koa');
-const router = require('./core/routes');
+const Router = require('./core/routes');
 
 const app = new Koa();
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(Router.routes());
+app.use(Router.allowedMethods());
 
 app.listen(process.env.SERVER_PORT, () => console.log(`Server running on ${process.env.SERVER_PORT}...`));
