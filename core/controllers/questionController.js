4db2f271ab8097091d84c2bd2559e14282c012e1
Wallef Reis Borges <reisborw@coventry.ac.uk>
2019-10-16 14:33:27 +0100

    changing files to match eslint, and adding some git hooks

--- core/controllers/questionController.js
+++ core/controllers/questionController.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
-const Question = require('../models/question');
+const Question = require('../models/question')
 
 module.exports = class QuestionController {
 
-    static showExample() {
-        let question = new Question();
-        console.log(question.example());
-        return question.example();
-    }
-};
+	static showExample() {
+		const question = new Question()
+		console.log(question.example())
+		return question.example()
+	}
+}
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
-require('dotenv').config();
-const Koa = require('koa');
-const Router = require('./core/routes');
+require('dotenv').config()
+const Koa = require('koa')
+const Router = require('./core/routes')
 
-const app = new Koa();
+const app = new Koa()
 
-app.use(Router.routes());
-app.use(Router.allowedMethods());
+app.use(Router.routes())
+app.use(Router.allowedMethods())
 
-app.listen(process.env.SERVER_PORT, () => console.log(`Server running on ${process.env.SERVER_PORT}...`));
+app.listen(process.env.SERVER_PORT, () => console.log(`Server running on ${process.env.SERVER_PORT}...`))
