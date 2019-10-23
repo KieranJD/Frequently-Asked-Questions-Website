551f26b10f9b266ee9effce1cf685b26cc5b19d6
Wallef Reis Borges <reisborw@coventry.ac.uk>
2019-10-23 15:23:56 +0100

    creating db file to help with database communication; dynamic title on all pages; starting answers module part2

---
+++ core/dbQueries.js
@@ -0,0 +1,35 @@
+'use strict'
+
+module.exports = class DbQueries {
+	static cerateUsersTable() {
+		// TO DO
+	}
+
+	static cerateQuestionsTable() {
+		// TO DO
+	}
+
+	static createAnswersTable() {
+		return `CREATE TABLE IF NOT EXISTS answers (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			description TEXT NOT NULL,
+			total_rate INTEGER,
+			rate_counter INTEGER,
+			average_rate REAL,
+			is_correct INTEGER DEFAULT 0,
+			is_inapropriate INTEGER DEFAULT 0,
+			user_id INTEGER NOT NULL,
+			question_id INTEGER NOT NULL,
+			FOREIGN KEY("user_id") REFERENCES "users"("id"),
+			FOREIGN KEY("question_id") REFERENCES "questions"("id")
+		);`
+	}
+
+	static select(select, table, condition) {
+		if (condition === undefined) {
+			return `SELECT ${select} FROM ${table}`
+		}
+
+		return `SELECT ${select} FROM ${table} WHERE ${condition}`
+	}
+}
