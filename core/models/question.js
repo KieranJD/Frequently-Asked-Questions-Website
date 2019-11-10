367e19add34c9621a2fc33f7cddad47e74709b6c
Kieran Dhir <dhirk@coventry.ac.uk>
2019-11-10 03:12:00 +0000

    Cleaned up questionRoutes.js
    Uneeded commented out code
    
    Removed commented out code to be ready to push to master branch

--- core/models/question.js
+++ core/models/question.js
@@ -6,15 +6,6 @@ const dbName = 'website.db'
 
 module.exports = class Question {
 
-	/*constructor(dbName = ':memory:') {
-		return (async() => {
-			this.db = await Database.open(dbName)
-			const sql = 'CREATE TABLE IF NOT EXISTS Questions (question_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, question TEXT)'
-			await this.db.run(sql)
-			return this
-		})()
-	}*/
-
 	async getAllQuestions(query) {
 		try {
 			let sql = 'SELECT question_id, title, question, solved, user_id, date FROM Questions;'
@@ -52,30 +43,5 @@ module.exports = class Question {
 		await db.run(sql)
 		await db.close()
 	}
-
-	/*async getAllQuestions(query) {
-		try {
-			let sql = 'SELECT question_id, title, question FROM Questions;'
-			console.log(query.search)
-			if(query !== undefined && query.search !== undefined) {
-				sql = `SELECT question_id, title, question FROM Questions
-								WHERE upper(title) LIKE "%${query.search}%";`
-			}
-			const data = await this.db.all(sql)
-			console.log(data)
-			return data
-		} catch(err) {
-			return err.message
-		}
-	}
-
-	async insertQuestion(request) {
-		console.log(request.body)
-		const body = request.body
-		const sql = `INSERT INTO Questions(title, question) 
-			VALUES("${body.title}", "${body.question}");`
-		console.log(sql)
-		await this.db.run(sql)
-	}*/
 
 }
