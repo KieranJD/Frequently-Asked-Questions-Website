dc4857f365c4f7a6358f0d3d388efa1f8da6719c
townse41 <townse41@coventry.ac.uk>
2019-11-20 20:43:28 +0000

    Added test to check if the username constains a space usernames should not be allowed to have a space in added test to user.spec.js to check the usernameValidation function if a space is in the username also changed function name checkNameInUser to usernameValidation

--- core/models/user.js
+++ core/models/user.js
@@ -19,7 +19,7 @@ module.exports = class User {
 		try {
 			this.mandatoryFieldsCheck(name, username, password)
 			let sql = await this.checkIfUsernameExists('register', username)
-			sql = await this.checkNameInUser(name, username)
+			sql = await this.usernameValidation(name, username)
 			password = await bcrypt.hash(password, saltRounds)
 			sql = `INSERT INTO users(name, username, password) VALUES("${name}", "${username}", "${password}")`
 
@@ -76,7 +76,7 @@ module.exports = class User {
 		return auth
 	}
 
-	async checkNameInUser(name, username) {
+	async usernameValidation(name, username) {
 		if (username.toUpperCase().includes(name.toUpperCase())) {
 			throw new Error('Username cannot include real name')
 		}
