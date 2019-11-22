a2e035c368da5f1501638b0544d5d092d21ea5f3
Wallef Reis Borges <reisborw@coventry.ac.uk>
2019-11-22 13:38:31 +0000

    100% test coverage for answers so far Created another unit test to make sure empty answers are not allowed. createAnswers method is now fully tested

--- unit test/answer.spec.js
+++ unit test/answer.spec.js
@@ -9,8 +9,8 @@ afterAll( async() => {
 	// runs after all the tests have completed
 })
 
-describe('insert()', () => {
-	test('create an answer', async done => {
+describe('Create()', () => {
+	test('Create an answer', async done => {
 		expect.assertions(1)
 		// ARRANGE
 		const answer = await new Answer()
@@ -23,6 +23,20 @@ describe('insert()', () => {
 		const insert = await answer.createAnswer(request, '21/11/2019')
 		// ASSERT
 		expect(insert).toBeTruthy()
+		done()
+	})
+
+	test('Empty answer', async done => {
+		expect.assertions(1)
+		//ARRANGE
+		const answer = await new Answer()
+		const request = {
+			body: {body: ''},
+			parameters: {question_id: 1},
+			session: {user: {id: 1}}
+		}
+		//ACT & ASSERT
+		await expect(answer.createAnswer(request, '21/11/2019')).rejects.toEqual(Error('Answer cannot be empty!'))
 		done()
 	})
 })
